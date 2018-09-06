e6010c5270d3b26174ab6bcea147d811610113ec
yy <yy@ityy.xyz>
2018-09-06 13:51:49 +0800

    change

---
+++ Linux_getch.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: Linux_getch.h
+    > Author: yy
+    > Mail: yy@ityy.xyz 
+    > Created Time: Thu 06 Sep 2018 01:46:06 PM CST
+ ************************************************************************/
+#ifndef _LINUX_GETCH_H_
+#define _LINUX_GETCH_H_
+
+int Linux_getch();
+
+#endif
+
